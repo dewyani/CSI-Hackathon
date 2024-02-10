78370497716d06f6366572b5cb65bccfa8f07584
priyankak3 <itspriyanka.kadam@spit.ac.in>
2024-02-10 22:34:50 +0530

    bhayanak flex

--- src/components/MyComplaints.jsx
+++ src/components/MyComplaints.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import SidebarPage from './SidebarPage'
-import { Card } from 'flowbite-react';
-import { Button, Modal, Timeline } from 'flowbite-react';
-import { useState } from 'react';
-import { HiArrowNarrowRight, HiCalendar } from 'react-icons/hi';
-import MyCard from './MyCard';
+import React from "react";
+import SidebarPage from "./SidebarPage";
+import { Card } from "flowbite-react";
+import { Button, Modal, Timeline } from "flowbite-react";
+import { useState } from "react";
+import { HiArrowNarrowRight, HiCalendar } from "react-icons/hi";
+import MyCard from "./MyCard";
 
-
-
-const MyComplaints = () => {
-    const [openModal, setOpenModal] = useState(false);
-    return (
-        <div className='flex'>
-            <SidebarPage />
-            <div className='flex-col m-10 space-y-2'>
-                <div className='font-bold text-xl text-gray-400'>In Progress</div>
-                <MyCard/>
-                <MyCard/>
-            </div>
-            <div className='flex-col m-10 space-y-2'>
-                <div className='font-bold text-xl text-gray-400'>Resolved Successfully</div>
-                <MyCard/>
-                <MyCard/>
-            </div>
+const MyComplaints = (props) => {
+  const [openModal, setOpenModal] = useState(false);
+  console.log(props.state[0]);
+  return (
+    <div className="flex">
+      <SidebarPage />
+      <div className="justify-center">Current Account : {props.state[0]}</div>
+      <div className="flex-col m-10 space-y-2">
+        <div className="font-bold text-xl text-gray-400">In Progress</div>
+        <MyCard />
+        <MyCard />
+      </div>
+      <div className="flex-col m-10 space-y-2">
+        <div className="font-bold text-xl text-gray-400">
+          Resolved Successfully
         </div>
-    )
-}
+        <MyCard />
+        <MyCard />
+      </div>
+    </div>
+  );
+};
 
-export default MyComplaints
+export default MyComplaints;
