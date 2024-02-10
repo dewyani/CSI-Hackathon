7fbc07d78e85203f6127b2c81fbb3ffeddb0a515
priyankak3 <itspriyanka.kadam@spit.ac.in>
2024-02-10 21:03:48 +0530

    dew

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { Navbar } from 'flowbite-react'
-import './App.css'
-import DashboardAgency from "./components/DashboardAgency"
-import DashboardUser from './components/DashboardUser'
-import Signup from './components/Signup'
-import Login from './components/Login'
-import History from './components/History'
-import {Routes , Route} from "react-router-dom"
-import MyComplaints from './components/MyComplaints'
-import LodgeComplaints from './components/LodgeComplaints'
-import Statistics from './components/Statistics'
-import Timeline from './components/Timeline'
+import { Navbar } from "flowbite-react";
+import "./App.css";
+import DashboardAgency from "./components/DashboardAgency";
+import DashboardUser from "./components/DashboardUser";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import History from "./components/History";
+import { Routes, Route } from "react-router-dom";
+import MyComplaints from "./components/MyComplaints";
+import LodgeComplaints from "./components/LodgeComplaints";
+import Statistics from "./components/Statistics";
+import Timeline from "./components/Timeline";
 
 function App() {
-
   return (
     <>
-    <div className='text-blue-500'>hjnkjagsrn</div>
-
-    <Routes>
-      <Route path="/login" element ={<Login/>}/>
-      <Route path="/dashboard_User" element ={<DashboardUser/>}/>
-      <Route path="/dashboard_Agency" element ={<DashboardAgency/>}/>
-      <Route path="/signup" element ={<Signup/>}/>
-      <Route path="/dashboard_User/history" element ={<History/>}/>
-      <Route path="/dashboard_User/myComplaints" element ={<MyComplaints/>}/>
-      <Route path="/dashboard_User/lodge" element ={<LodgeComplaints/>}/>
-      <Route path="/dashboard_Agency/stats" element ={<Statistics/>}/>
-      <Route path="/dashboard_Agency/timeline" element ={<Timeline/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard_User" element={<DashboardUser />} />
+        <Route path="/dashboard_Agency" element={<DashboardAgency />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard_User/history" element={<History />} />
+        <Route path="/dashboard_User/myComplaints" element={<MyComplaints />} />
+        <Route path="/dashboard_User/lodge" element={<LodgeComplaints />} />
+        <Route path="/dashboard_Agency/stats" element={<Statistics />} />
+        <Route path="/dashboard_Agency/timeline" element={<Timeline />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
