7b69b53af409206c503764a2e8f09234a4a37f36
dewyani <dadeshmukh1411@gmail.com>
2024-02-11 01:09:42 +0530

    dimag kharab ho raha hai

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
 
 const Signup = () => {
 
@@ -94,7 +93,7 @@ const Signup = () => {
                 Create an account
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Already have an account? <a href="" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                Already have an account? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
               </p>
             </form>
           </div>
