0d0862509e640e27099807f10feae12e4e8f8a50
dewyani <dadeshmukh1411@gmail.com>
2024-02-10 20:23:53 +0530

    login page

--- src/components/Login_Navbar.jsx
+++ src/components/Login_Navbar.jsx
@@ -6,25 +6,17 @@ const Login_Navbar = () => {
     <>
       <Navbar fluid rounded>
       <Navbar.Brand href="https://flowbite-react.com">
-        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+        
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Support Sync</span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Button>Get started</Button>
+        <Button>Login</Button>
         <Navbar.Toggle />
       </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
-      </Navbar.Collapse>
+      
     </Navbar>
     </>
   )
 }
 
-export default Navbar
+export default Login_Navbar
