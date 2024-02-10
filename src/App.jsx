11dc3eea6d86b115ba9c031e3205667321c9ce57
neerja-1984 <nirjadoshi2003@gmail.com>
2024-02-10 16:02:32 +0530

    helo

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,11 @@ function App() {
   return (
     <>
     <div className='text-blue-500'>hello</div>
-    
-
-
     <Routes>
       <Route path="/login" element ={<Login/>}/>
       <Route path="/dashboard_User" element ={<DashbaoardUser/>}/>
       <Route path="/dashboard_Agency" element ={<DashbaoardAgency/>}/>
       <Route path="/signup" element ={<Signup/>}/>
-  
     </Routes>
     </>
   )
