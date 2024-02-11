d43f6258d85824a2ed010828576762977fd70cf2
apulkid154 <apulki.dubey@spit.ac.in>
2024-02-11 10:13:35 +0530

    final changes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -20,7 +20,8 @@ const Signup = () => {
 
   return (
     <section>
-      <div className="flex flex-col items-center justify-center px-6 mt-16" >
+      <div className="flex flex-col items-center  px-6 mt-16" >
+       
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-2 md:space-y-6 sm:p-8 border-black-500 shadow-2xl">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
